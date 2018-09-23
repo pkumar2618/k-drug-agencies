7e3b26a16d3665d90d3d11375802592f4b49a6c7
pawan kumar <pawan.26888@gmail.com>
2018-09-24 01:56:33 +0530

    Initial Commit

---
+++ k-drug-rings/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  k-drug-rings
+//
+//  Created by pawan kumar on 24/09/18.
+//  Copyright © 2018 iit-delhi. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
