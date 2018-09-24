504cd035c2bd90abc63c6357e29454b5d4daae26
Pawan Kumar <pawan.kumar@cse.iitd.ac.in>
2018-09-24 12:32:18 +0530

    Starting off with git repo

--- k-drug-rings/main.cpp
+++ k-drug-rings/main.cpp
@@ -8,8 +8,9 @@
 
 #include <iostream>
 
+
 int main(int argc, const char * argv[]) {
     // insert code here...
-    std::cout << "Hello, World!\n";
+    std::cout << "Hello, World!\n"<<std::endl;
     return 0;
 }
